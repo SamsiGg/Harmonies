ac27fc679fca7e6cd38f738572577cce8f3a111b
Samuel Geffert <samuelgeffert@gmail.com>
2025-09-09 11:39:11 +0200

    Initialisation of GameState finished

---
+++ GameEngine.py
@@ -0,0 +1,15 @@
+class GameEngine():
+
+    def __init__():
+
+    @staticmethod
+    def get_valid_moves():
+
+    @staticmethod
+    def apply_move():
+
+    @staticmethod
+    def calculate_scores():
+
+    @staticmethod
+    def check_end_condition():
